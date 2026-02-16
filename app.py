e99221343ecbaeb18cf27f530e2d21984fa92a69
MD <iammd5679@gmail.com>
2026-02-16 12:01:22 +0530

    feat: Add page config and title

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 import langchain
 from langchain_text_splitters import RecursiveCharacterTextSplitter, CharacterTextSplitter
+
+st.set_page_config(page_title="Text Splitter Playground", layout="wide")
+st.title("Text Splitter Visualization")
